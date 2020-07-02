beee07d022cc2c675cdd3fb71f4d6e2b934e147a
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-07-02 09:41:36 -0400

    BroswerRouter basename fix for prod

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import { BrowserRouter } from 'react-router-dom';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import store, { history } from './util/store';
 import NavigationBar from './features/navigation/index';
@@ -10,12 +10,12 @@ import './img/favicon.ico';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
+    <BrowserRouter basename="/brigandine-helper/" history={history}>
       <div>
         <NavigationBar />
         <Routes />
       </div>
-    </ConnectedRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('app'),
 );
