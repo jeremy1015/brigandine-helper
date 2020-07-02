14fa4c2f9695f5cb24bf4af23aa8824fa759b1aa
Phuc Khuu <phuc_khuu@hotmail.com>
2020-07-02 23:39:11 +0700

    Data entry for class and 10 first location
    Data from https://docs.google.com/spreadsheets/d/1phCX__6IuomaEnioApz_db_ZNaA7xwPazKwJoHDqhIY/edit#gid=469161403

--- src/data/classes.js
+++ src/data/classes.js
@@ -8,7 +8,7 @@ export default {
       lt.highlands,
       lt.sanctuary,
       lt.shrine,
-      lt.fortPastous,
+      lt.fortPastous
     ],
   },
   swordsman: {
@@ -18,7 +18,158 @@ export default {
       lt.jungle,
       lt.woodland,
       lt.isleOfTrembo,
-      lt.ivoryDragonSpring,
+      lt.ivoryDragonSpring
+    ],
+  },
+  berserker: {
+    name: 'Berserker',
+    questBonus: [
+      lt.plains,
+      lt.highlands
+    ],
+  },
+  viking: {
+    name: 'Viking',
+    questBonus: [
+      lt.lakeside,
+      lt.valley,
+      lt.falls,
+      lt.strait,
+      lt.shrine,
+      lt.bay
+    ],
+  },
+  ranger: {
+    name: 'Ranger',
+    questBonus: [
+      lt.ruins,
+      lt.fort,
+      lt.wasteland,
+      lt.lakeside,
+      lt.valley,
+      lt.falls,
+      lt.peaks,
+      lt.mount,
+      lt.hill,
+      lt.alps,
+      lt.forest,
+      lt.jungle,
+      lt.woodland,
+      lt.sanctuary,
+      lt.shrine,
+      lt.fortPastous,
+      lt.strait,
+      lt.sea,
+      lt.bay,
+      lt.isleOfTrembo,
+      lt.isleOfMartha,
+      lt.isleOfLennu,
+      lt.manaSpring,
+      lt.hiddenTreasury
+    ],
+  },
+  grappler: {
+    name: 'Grappler',
+    questBonus: [
+      lt.peaks,
+      lt.mount,
+      lt.hill,
+      lt.alps
+    ],
+  },
+  sorcerer: {
+    name: 'Sorcerer',
+    questBonus: [
+      lt.ruins,
+      lt.fort,
+      lt.wasteland,
+      lt.plains,
+      lt.highlands,
+      lt.fortPastous
+    ],
+  },
+  bishop: {
+    name: 'Bishop',
+    questBonus: [
+      lt.plains,
+      lt.highlands,
+      lt.sanctuary,
+      lt.shrine,
+      lt.hiddenTreasury
+    ],
+  },
+  templeKnight: {
+    name: 'Temple Knight',
+    questBonus: [
+      lt.lakeside,
+      lt.valley,
+      lt.falls,
+      lt.ivoryDragonSpring
+    ],
+  },
+  archer: {
+    name: 'Archer',
+    questBonus: [
+      lt.forest,
+      lt.jungle,
+      lt.woodland,
+      lt.isleOfTrembo,
+      lt.ivoryDragonSpring
+    ],
+  },
+  minstrel: {
+    name: 'Minstrel',
+    questBonus: [
+      lt.forest,
+      lt.jungle,
+      lt.woodland,
+      lt.isleOfTrembo,
+      lt.ivoryDragonSpring
+    ],
+  },
+  rogue: {
+    name: 'Rogue',
+    questBonus: [
+      lt.ruins,
+      lt.fort,
+      lt.wasteland,
+      lt.peaks,
+      lt.mount,
+      lt.hill,
+      lt.alps,
+      lt.fortPastous
+    ],
+  },
+  sorcereress: {
+    name: 'Sorceress',
+    questBonus: [
+      lt.plains,
+      lt.highlands,
+      lt.fortPastous
+    ],
+  },
+  healer: {
+    name: 'Healer',
+    questBonus: [
+      lt.plains,
+      lt.highlands,
+      lt.sanctuary,
+      lt.shrine,
+      lt.hiddenTreasury
+    ],
+  },
+  heroOfOld: {
+    name: 'Hero of Old',
+    questBonus: [
+      lt.peaks,
+      lt.mount,
+      lt.hill,
+      lt.alps,
+      lr.sanctuary,
+      lt.shrine,
+      lt.isleOfMartha,
+      lt.isleOfLennu,
+      lt.manaSpring
     ],
   },
 };
