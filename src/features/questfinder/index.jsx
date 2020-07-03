5928ff9537687ff5b9b01f1084e09e109164345b
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-07-03 00:42:40 -0400

    Cities added. Now filterable by city.

--- src/features/questfinder/index.jsx
+++ src/features/questfinder/index.jsx
@@ -1,29 +1,38 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { Container, Card, CardHeader, CardBody, CardTitle, ListGroup, ListGroupItem, ListGroupItemHeading, Row, Col } from 'reactstrap';
+import { Container, Card, CardHeader, CardBody, CardTitle, ListGroup, ListGroupItem, ListGroupItemHeading, Row, Col, Alert } from 'reactstrap';
 import { Typeahead } from 'react-bootstrap-typeahead';
 
 import classes from '../../data/classes';
 import rewardTypes from '../../data/reward-types';
 import locations from '../../data/locations';
 import cities from '../../data/cities';
+import factions from '../../data/factions';
 
 const classArr = Object.values(classes);
 const rewardArr = Object.values(rewardTypes);
 const locationArr = Object.values(locations);
 const cityArr = Object.values(cities);
-
-const citiesForLocations = _.reduce(locationArr, (m, location) => ({ ...m, [location.name]: _.filter(cityArr, c => c.locations.includes(location)) }), {}); 
+const factionsArr = Object.values(factions);
 
 const containerStyle = {
   marginTop: '10px',
 };
 
 const Questfinder = () => {
+  const locsByCity = cArray => _.reduce(locationArr, (m, location) => ({ ...m, [location.name]: _.filter(cArray, c => c.locations.includes(location)) }), {});
   const [selectedClass, setSelectedClass] = useState([]);
   const [selectedRewards, setSelectedRewards] = useState([]);
+  const [selectedCities, setSelectedCities] = useState([...cityArr]);
+  const [availableCities, setAvailableCities] = useState(locsByCity(cityArr));
   const [queryResults, setQueryResults] = useState([]);
+
+  const cityToggle = (c) => {
+    const newCities = selectedCities.includes(c) ? _.without(selectedCities, c) : [...selectedCities, c];
+    setSelectedCities(newCities);
+    setAvailableCities(locsByCity(newCities));
+  };
 
   useEffect(() => {
     if (!(selectedClass && selectedClass[0] && selectedRewards && selectedRewards[0])) {
@@ -39,39 +48,59 @@ const Questfinder = () => {
     }));
     console.log(results);
     setQueryResults(results);
-  }, [selectedClass, selectedRewards]);
+  }, [selectedClass, selectedRewards, selectedCities]);
   return (
     <Container style={containerStyle}>
-      <h3>Quest Location Finder</h3>
-      <p>Select a class, then if desired select a loot type. The questfinder will suggest the optimal locations for your questing.</p>
-      <Row>
-        <Col sm="6">
-          <Typeahead
-            id="class-typeahead"
-            labelKey="name"
-            multiple
-            options={classArr}
-            placeholder="Choose one or more classes..."
-            onChange={setSelectedClass}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm="6">
-          <Typeahead
-            id="reward-typeahead"
-            labelKey="name"
-            options={rewardArr}
-            multiple
-            placeholder="Choose some quest objectives..."
-            onChange={setSelectedRewards}
-          />
-        </Col>
-      </Row>
+      <h3 className="text-center">Quest Location Finder</h3>
       <hr />
-      <div>
-        {(!(queryResults && queryResults[0])) && 'No Results'}
-        {(queryResults && queryResults[0]) 
+      <Row>
+        <Col sm="3">
+          <h4>Cities</h4>
+          <ListGroup>
+            {factionsArr.map(f => (
+              <Fragment key={f.name}>
+                <ListGroupItem>
+                  <ListGroupItemHeading>{f.name}</ListGroupItemHeading>
+                </ListGroupItem>
+                {f.starterCities.map(c => (
+                  <ListGroupItem key={c.name} tag="a" href="#" onClick={() => cityToggle(c)} color={selectedCities.includes(c) ? 'success' : 'secondary'}>
+                    {c.name}
+                  </ListGroupItem>
+                ))}
+              </Fragment>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col sm="9">
+          <Row>
+            <Col sm="12">
+              <h4>Selections</h4>
+              <Typeahead
+                id="class-typeahead"
+                labelKey="name"
+                multiple
+                options={classArr}
+                placeholder="Choose one or more classes..."
+                onChange={setSelectedClass}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="12">
+              <Typeahead
+                id="reward-typeahead"
+                labelKey="name"
+                options={rewardArr}
+                multiple
+                placeholder="Choose some quest objectives..."
+                onChange={setSelectedRewards}
+              />
+            </Col>
+          </Row>
+          <hr />
+          <div>
+            {(!(queryResults && queryResults[0])) && 'No Results'}
+            {(queryResults && queryResults[0]) 
           && queryResults.map(r => (
             <div>
               <h3>{r.name}</h3>
@@ -85,8 +114,8 @@ const Questfinder = () => {
                         {reward.locationMatches.map(m => (
                           <Fragment>
                             <ListGroup flush>
-                              <ListGroupItemHeading>Quest Site - {m.name}</ListGroupItemHeading>
-                              {citiesForLocations[m.name].map(city => (<ListGroupItem>{city.name}</ListGroupItem>))}
+                              <ListGroupItemHeading>{m.name}</ListGroupItemHeading>
+                              {availableCities[m.name].map(city => (<ListGroupItem>{city.name}</ListGroupItem>))}
                             </ListGroup>
                           </Fragment>
                         ))}
@@ -98,7 +127,9 @@ const Questfinder = () => {
               <hr />
             </div>
           ))}
-      </div>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
