e052774aaa15fb2d76de211fb62a4f1d8d1cc9ca
Chuck Logan Lim <chucklogan.lim@gmail.com>
2020-07-03 16:06:16 +1000

    Add locations without reward types

--- src/data/locations.js
+++ src/data/locations.js
@@ -57,4 +57,190 @@ export default {
     type: lt.isleOfLennu,
     rewards: [rt.horseshoe, rt.monsterBracer, rt.monsterLeather, rt.monsterAccessory, rt.crystal],
   },
+  isleOfMartha: {
+    name: 'Isle of Martha',
+    type: lt.isleOfMartha,
+    rewards: [],
+
+  },
+  isleOfSeymour: {
+    name: 'Isle of Seymour',
+    type: lt.isleOfSeymour,
+    rewards: [],
+
+  },
+  isleOfTrembo: {
+    name: 'Isle of Trembo',
+    type: lt.isleOfTrembo,
+    rewards: [],
+
+  },
+  ivoryDragonSpring: {
+    name: 'Ivory Dragon Spring',
+    type: lt.ivoryDragonSpring,
+    rewards: [],
+
+  },
+  lanardForest: {
+    name: 'Lanard Forest',
+    type: lt.lanardForest,
+    rewards: [],
+
+  },
+  mastonRange: {
+    name: 'Maston Range',
+    type: lt.mastonRange,
+    rewards: [],
+
+  },
+  morrowBay: {
+    name: 'Morrow Bay',
+    type: lt.morrowBay,
+    rewards: [],
+
+  },
+  mountMadroal: {
+    name: 'Mount Madroal',
+    type: lt.mountMadroal,
+    rewards: [],
+
+  },
+  mountRhode: {
+    name: 'Mount Rhode',
+    type: lt.mountRhode,
+    rewards: [],
+
+  },
+  neilsRuins: {
+    name: 'Neils Ruins',
+    type: lt.neilsRuins,
+    rewards: [],
+
+  },
+  ninfusSanctuary: {
+    name: 'Ninfu\'s Sanctuary',
+    type: lt.ninfusSanctuary,
+    rewards: [],
+
+  },
+  orsonBay: {
+    name: 'Orson Bay',
+    type: lt.orsonBay,
+    rewards: [],
+
+  },
+  peaksOfMaydel: {
+    name: 'Peaks of Maydel',
+    type: lt.peaksOfMaydel,
+    rewards: [],
+
+  },
+  plainsOfBlume: {
+    name: 'Plains of Blume',
+    type: lt.plainsOfBlume,
+    rewards: [],
+
+  },
+  plainsOfDelza: {
+    name: 'Plains of Delza',
+    type: lt.plainsOfDelza,
+    rewards: [],
+
+  },
+  plainsOfGaral: {
+    name: 'Plains of Garal',
+    type: lt.plainsOfGaral,
+    rewards: [],
+
+  },
+  plainsOfKarina: {
+    name: 'Plains of Karina',
+    type: lt.plainsOfKarina,
+    rewards: [],
+
+  },
+  poudValley: {
+    name: 'Poud Valley',
+    type: lt.poudValley,
+    rewards: [],
+
+  },
+  ruinsOfDapit: {
+    name: 'Ruins of Dapit',
+    type: lt.ruinsOfDapit,
+    rewards: [],
+
+  },
+  ruinsOfNarm: {
+    name: 'Ruins of Narm',
+    type: lt.ruinsOfNarm,
+    rewards: [],
+
+  },
+  ruinsOfShlay: {
+    name: 'Ruins of Shlay',
+    type: lt.ruinsOfShlay,
+    rewards: [],
+
+  },
+  rumiValley: {
+    name: 'Rumi Valley',
+    type: lt.rumiValley,
+    rewards: [],
+
+  },
+  seaOfMarianna: {
+    name: 'Sea of Marianna',
+    type: lt.seaOfMarianna,
+    rewards: [],
+
+  },
+  shamlahFalls: {
+    name: 'Shamlah Falls',
+    type: lt.shamlahFalls,
+    rewards: [],
+
+  },
+  shlayneStrait: {
+    name: 'Shlayne Strait',
+    type: lt.shlayneStrait,
+    rewards: [],
+
+  },
+  silvanLakeside: {
+    name: 'Silvan Lakeside',
+    type: lt.silvanLakeside,
+    rewards: [],
+
+  },
+  sintoGreatPlains: {
+    name: 'Sinto Great Plains',
+    type: lt.sintoGreatPlains,
+    rewards: [],
+
+  },
+  theManaSpring: {
+    name: 'The Mana Spring',
+    type: lt.theManaSpring,
+    rewards: [],
+
+  },
+  tilamValley: {
+    name: 'Tilam Valley',
+    type: lt.tilamValley,
+    rewards: [],
+
+  },
+  twinlilShrine: {
+    name: 'Twinlil Shrine',
+    type: lt.twinlilShrine,
+    rewards: [],
+
+  },
+  woodlandsOfLem: {
+    name: 'Woodlands of Lem',
+    type: lt.woodlandsOfLem,
+    rewards: [],
+
+  },
 };
