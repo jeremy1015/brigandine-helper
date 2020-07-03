ff2ba28a58d38aba8ac03c5ac2c1652156f8efa2
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-07-02 23:29:17 -0400

    Added factions, added all cities (locations not done for cities)

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,9 +1,165 @@
 import locs from './locations';
 
 export default {
+  warren: {
+    name: 'Warren',
+    locations: [],
+  },
+  belfaram: {
+    name: 'Belfaram',
+    locations: [],
+  },
+  dreug: {
+    name: 'Dreug',
+    locations: [],
+  },
+  greensglade: {
+    name: 'Greensglade',
+    locations: [],
+  },
+  zagust: {
+    name: 'Zagust',
+    locations: [],
+  },
+  garwellin: {
+    name: 'Garwellin',
+    locations: [],
+  },
+  lorentz: {
+    name: 'Lorentz',
+    locations: [],
+  },
+  marmelmo: {
+    name: 'Marmelmo',
+    locations: [],
+  },
+  lanster: {
+    name: 'Lanster',
+    locations: [],
+  },
+  angrein: {
+    name: 'Angrein',
+    locations: [],
+  },
+  harmonia: {
+    name: 'Harmonia',
+    locations: [],
+  },
+  fupai: {
+    name: 'Fupai',
+    locations: [],
+  },
+  tian: {
+    name: 'Tian',
+    locations: [],
+  },
+  mezza: {
+    name: 'Mezza',
+    locations: [],
+  },
+  katchana: {
+    name: 'Katchana',
+    locations: [],
+  },
+  koaloopa: {
+    name: 'Koaloopa',
+    locations: [],
+  },
+  minz: {
+    name: 'Minz',
+    locations: [],
+  },
+  milveen: {
+    name: 'Milveen',
+    locations: [],
+  },
+  ilvanny: {
+    name: 'Ilvanny',
+    locations: [],
+  },
+  saintGladia: {
+    name: 'Saint Gladia',
+    locations: [],
+  },
+  balutza: {
+    name: 'Balutza',
+    locations: [],
+  },
+  portside: {
+    name: 'Portside',
+    locations: [],
+  },
+  glan: {
+    name: 'Glan',
+    locations: [],
+  },
   alternia: {
     name: 'Alternia',
     locations: [locs.ariaGreatPlains],
+  },
+  sedestoria: {
+    name: 'Sedestoria',
+    locations: [],
+  },
+  naridge: {
+    name: 'Naridge',
+    locations: [],
+  },
+  zai: {
+    name: 'Zai',
+    locations: [],
+  },
+  ragrunt: {
+    name: 'Ragrunt',
+    locations: [],
+  },
+  galest: {
+    name: 'Galest',
+    locations: [],
+  },
+  saladiel: {
+    name: 'Saladiel',
+    locations: [],
+  },
+  norbass: {
+    name: 'Norbass',
+    locations: [],
+  },
+  anthelnia: {
+    name: 'Anthelnia',
+    locations: [],
+  },
+  vestillis: {
+    name: 'Vestillis',
+    locations: [],
+  },
+  cornwern: {
+    name: 'Cornwern',
+    locations: [],
+  },
+  ellandol: {
+    name: 'Ellandol',
+    locations: [],
+  },
+  gharghar: {
+    name: 'Gharghar',
+    locations: [],
+  },
+  meraniel: {
+    name: 'Meraniel',
+    locations: [],
+  },
+  orsol: {
+    name: 'Orsol',
+    locations: [],
+  },
+  angela: {
+    name: 'Angela',
+    locations: [],
+  },
+  shumenly: {
+    name: 'Shumenly',
+    locations: [],
   },
   drowarn: {
     name: 'Drowarn',
---
+++ src/data/factions.js
@@ -0,0 +1,28 @@
+import c from './cities';
+
+export default {
+  gustava: {
+    name: 'Gustava',
+    starterCities: [c.warren, c.belfaram, c.dreug, c.greensglade, c.zagust, c.garwellin],
+  },
+  norzaleo: {
+    name: 'Norzaleo',
+    starterCities: [c.lorentz, c.marmelmo, c.lanster, c.angrein, c.harmonia],
+  },
+  shinobi: {
+    name: 'Shinobi',
+    starterCities: [c.fupai, c.tian, c.mezza, c.katchana, c.koaloopa],
+  },
+  mirelva: {
+    name: 'Mirelva',
+    starterCities: [c.minz, c.milveen, c.ilvanny, c.saintGladia, c.balutza, c.portside, c.glan],
+  },
+  manaSaleesia: {
+    name: 'Mana Saleesia',
+    starterCities: [c.alternia, c.sedestoria, c.naridge, c.zai, c.ragrunt, c.galest, c.saladiel, c.norbass, c.anthelnia, c.vestillis],
+  },
+  guimoule: {
+    name: 'Guimoule',
+    starterCities: [c.cornwern, c.ellandol, c.gharghar, c.meraniel, c.orsol, c.angela, c.shumenly, c.drowarn],
+  },
+};
