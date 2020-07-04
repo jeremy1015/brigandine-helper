da09aad250ad1c5f93e5316577694711ce0dd63b
tehjv <jv_coronel@hotmail.com>
2020-07-04 18:52:33 +0800

    add knights list
    - add norzaleo knights
    - add guimoule knights

---
+++ src/data/knights.js
@@ -0,0 +1,132 @@
+import f from './factions';
+
+/* Knight Data Model
+    Feel free to suggest changes. I'm just using 
+    knightName: {
+        faction: f.factionObject.name
+        maxManaPool : number //median from the excel provided from a tw forum
+        startingLocation: cityObject.name
+        // uhh i got lazy gonna add data now...
+    }
+*/
+
+export default {
+  Rubino: {
+    faction: f.norzaleo.name,
+    maxManaPool: 390,
+  },
+  Elena: {
+    faction: f.norzaleo.name,
+    maxManaPool: 380,
+  },
+  Leonora: {
+    faction: f.norzaleo.name,
+    maxManaPool: 350,
+  },
+  Ariana: {
+    faction: f.norzaleo.name,
+    maxManaPool: 340,
+  },
+  Pick: {
+    faction: f.norzaleo.name,
+    maxManaPool: 330,
+  },
+  Grados: {
+    faction: f.norzaleo.name,
+    maxManaPool: 320,
+  },
+  Theodora: {
+    faction: f.norzaleo.name,
+    maxManaPool: 320,
+  },
+  Schizler: {
+    faction: f.norzaleo.name,
+    maxManaPoo: 300,
+  },
+  Jack: {
+    faction: f.norzaleo.name,
+    maxManaPool: 300,
+  },
+  Maximilian: {
+    faction: f.norzaleo.name,
+    maxManaPool: 290,
+  },
+  Brendan: {
+    faction: f.norzaleo.name,
+    maxManaPool: 280,
+  },
+  Ferrick: {
+    faction: f.norzaleo.name,
+    maxManaPool: 280,
+  },
+  Jiu: {
+    faction: f.norzaleo.name,
+    maxManaPool: 270,
+  },
+  Klaus: {
+    faction: f.norzaleo.name,
+    maxManaPool: 260,
+  },
+  
+  Eliza: {
+    maxManaPool: 380,
+    faction: f.guimoule.name,
+  },
+  Sugar: {
+    maxManaPool: 360,
+    faction: f.guimoule.name,
+  },
+  Cain: {
+    maxManaPool: 350,
+    faction: f.guimoule.name,
+  },
+  Patricia: {
+    maxManaPool: 350,
+    faction: f.guimoule.name,
+  },
+  Darian: {
+    maxManaPool: 330,
+    faction: f.guimoule.name,
+  },
+  Kate: {
+    maxManaPool: 320,
+    faction: f.guimoule.name,
+  },
+  Leanne: {
+    maxManaPool: 320,
+    faction: f.guimoule.name,
+  },
+  Conrad: {
+    maxManaPool: 300,
+    faction: f.guimoule.name,
+  },
+  Marcosias: {
+    maxManaPool: 300,
+    faction: f.guimoule.name,
+  },
+  Rose: {
+    maxManaPool: 280,
+    faction: f.guimoule.name,
+  },
+  Diana: {
+    maxManaPool: 280,
+    faction: f.guimoule.name,
+  },
+  "Mu'ah": {
+    maxManaPool: 278,
+    faction: f.guimoule.name,
+  },
+  Vayne: {
+    maxManaPool: 267,
+    faction: f.guimoule.name,
+  },
+  Avenir: {
+    maxManaPool: 260,
+    faction: f.guimoule.name,
+  },
+  Faye: {
+    maxManaPool: 220,
+    faction: f.guimoule.name,
+  },
+
+};
