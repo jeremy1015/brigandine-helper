57d6a7f680293aa212f80978b42bf628f14a1eb3
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-07-04 05:55:56 -0400

    Navigation Links

--- src/features/navigation/index.jsx
+++ src/features/navigation/index.jsx
@@ -4,10 +4,17 @@ import {
   Navbar,
   NavbarBrand,
   Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavLink,
+  NavItem,
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import routes from '../../util/routes';
 import eliza from '../../img/eliza.png';
-
 
 const NavigationBar = () => (
   <Navbar color="light" expand="sm">
@@ -16,7 +23,35 @@ const NavigationBar = () => (
       &nbsp;Brigandine Helper
     </NavbarBrand>
     <Collapse navbar>
-      <Nav className="mr-auto" navbar />
+      <Nav className="mr-auto" navbar>
+        <UncontrolledDropdown inNavbar nav>
+          <DropdownToggle nav caret>
+            Useful Links
+          </DropdownToggle>
+          <DropdownMenu>
+            <NavLink target="_blank" href="https://discord.gg/8CF38Qe">
+              <DropdownItem>
+                Brigandine Discord
+              </DropdownItem>
+            </NavLink>
+            <NavLink target="_blank" href="https://docs.google.com/spreadsheets/d/1phCX__6IuomaEnioApz_db_ZNaA7xwPazKwJoHDqhIY">
+              <DropdownItem>
+                Runersia Stat Tracker by Lasci
+              </DropdownItem>
+            </NavLink>
+            <NavLink target="_blank" href="https://brigandine.fandom.com/wiki/Brigandine:_The_Legend_of_Runersia">
+              <DropdownItem>
+                Brigandine Fandom Wiki
+              </DropdownItem>
+            </NavLink>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+        <NavItem>
+          <NavLink tag={Link} to={routes.questfinder}>
+            Location Questfinder
+          </NavLink>
+        </NavItem>
+      </Nav>
     </Collapse>
   </Navbar>
 );
