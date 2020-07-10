1e2d2ca0e7dbd3e7fa013bc7a4626e4d32f9b302
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-07-10 07:22:23 -0400

    Moving sorting and sort order to column headers, improved factions filter, simplified filter

--- src/features/knightbrowser/index.jsx
+++ src/features/knightbrowser/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import {
-  Form, Container, FormGroup, Label, CardBody, Table,
-  Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
+  Form, Container, FormGroup, Label, Table,
+  Row, Col,
 } from 'reactstrap';
-import _, { get } from 'lodash';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortUp, faSortDown } from '@fortawesome/pro-solid-svg-icons';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import knights from '../../data/knights';
 import factions from '../../data/factions';
@@ -12,79 +15,121 @@ import KnightCard from '../knightcard';
 const factionsArr = Object.values(factions);
 const knightsArr = Object.values(knights);
 
+const pt = { cursor: 'pointer' };
+
+const knightProps = {
+  name: { name: 'Name', prop: 'name' },
+  faction: { name: 'Faction', prop: 'faction.name' },
+  mmp30: { name: 'Median Mana Pool at Level 30', prop: 'maxManaPool' },
+};
+
+const knightPropsArr = Object.values(knightProps);
+
 const Knightbrowser = () => {
-  const knightProps = Object.keys(knightsArr[0]);
   const [displayedKnights, setDisplayedKnights] = useState([...knightsArr]);
-  const [factionFilter, setFactionFilter] = useState('All');
-  const [sortProp, setSortProp] = useState(knightProps[0]);
+  const [sortProp, setSortProp] = useState(knightPropsArr[0]);
   const [sortOrder, setSortOrder] = useState('asc');
-  const changeFactionFilter = e => setFactionFilter(e.target.value);
-  const changeSortProp = e => setSortProp(e.target.value);
-  const changeSortOrder = e => setSortOrder(e.target.value);
+
+  const [selectedFactions, setSelectedFactions] = useState([]);
+
+  const order = (prop) => {
+    if (prop === sortProp) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+      return;
+    }
+    setSortProp(prop);
+    setSortOrder('asc');
+  };
+
+  const filterKnights = () => {
+    const fToUse = _.isEmpty(selectedFactions) ? factionsArr : selectedFactions;
+    return knightsArr.filter(k => fToUse.includes(k.faction));
+  };
+
+  const sortKnights = (kArr) => {
+    if (sortProp.name === 'growthRate') {
+      return _.orderBy(kArr, k => (k.growthRate === 'S' ? ' ' : k.growthRate));
+    } 
+    if (sortProp.name === 'growth') {
+      return _.orderBy(kArr, k => (k.growth === 'S' ? ' ' : k.growth));
+    }
+    return _.orderBy(kArr, k => _.get(k, sortProp.prop), sortOrder);
+  };
+
+  useEffect(() => {
+    if (!selectedFactions || !sortProp) return;
+    setDisplayedKnights(sortKnights(filterKnights()));
+  }, [selectedFactions, sortProp, sortOrder]);
 
   return (
     <Container fluid className="mt-3 pl-3 pr-3">
       <KnightCard knight={knights.Avenir} />
       <h3 className="text-center">Knight Browser</h3>
+      <Form>
+        <FormGroup row>
+          <Col sm={4}>
+            <Typeahead
+              id="class-typeahead"
+              labelKey="name"
+              multiple
+              options={factionsArr}
+              placeholder="Filter by factions..."
+              onChange={setSelectedFactions}
+              className="mb-2"
+            />
+          </Col>
+        </FormGroup>
+      </Form>
       <Row>
         <Col sm="12">
-          <Form>
-            <FormGroup>
-              <Label for="faction-select">Faction filter: </Label>
-              <select className="mr-3" id="faction-select" onChange={changeFactionFilter} value={factionFilter}>
-                <option key="All" value="All">All</option>
-                {factionsArr.map(faction => (<option key={faction.name} value={faction.name}>{faction.name}</option>))}
-              </select>
-              <Label for="sort-select">Sort by: </Label>
-              <select className="mr-3" id="sort-select" onChange={changeSortProp} value={sortProp}>
-                {knightProps.map(prop => (<option key={prop} value={prop}>{prop}</option>))}
-              </select>
-              <Label for="sort-select">Order by: </Label>
-              <select id="sort-select" onChange={changeSortOrder} value={sortOrder}>
-                <option key="asc" value="asc">Ascending</option>
-                <option key="desc" value="desc">Descending</option>
-              </select>
-            </FormGroup>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        <Col sm="12">
-          <Table striped responsive>
+          <Table hover striped responsive>
             <thead>
-              <tr>
-                <th>Name</th>
-                <th>Faction</th>
-                <th>Median Max Mana Pool</th>
+              <tr className={pt}>
+                <th onClick={() => order(knightProps.name)}>
+                  Name
+                  {sortProp === knightProps.name 
+                    && (<FontAwesomeIcon className="float-right" icon={sortOrder === 'asc' ? faSortUp : faSortDown} />)}
+                </th>
+                <th onClick={() => order(knightProps.faction)}>
+                  Faction
+                  {sortProp === knightProps.faction 
+                    && (<FontAwesomeIcon className="float-right" icon={sortOrder === 'asc' ? faSortUp : faSortDown} />)}
+                </th>
+                <th>Class</th>
+                <th>Level</th>
+                <th>Growth</th>
+                <th>Growth Rate</th>
+                <th>HP</th>
+                <th>ATK</th>
+                <th>DEF</th>
+                <th>STR</th>
+                <th>INT</th>
+                <th>AGI</th>
+                <th>Pool</th>
+                <th>30 Pool</th>
+                <th>RA</th>
               </tr>
             </thead>
             <tbody>
               {displayedKnights
-                .sort((knight1, knight2) => {
-                  const sorBy = sortProp === 'faction' ? `${sortProp}.name` : sortProp;
-                  const val1 = _.get(knight1, sorBy, '');
-                  const val2 = _.get(knight2, sorBy, '');
-                  
-                  if (val1 < val2) {
-                    return sortOrder === 'asc' ? -1 : 1;
-                  }
-                  if (val1 > val2) {
-                    return sortOrder === 'asc' ? 1 : -1;
-                  }
-                  return 0;
-                })
-                .filter((knight) => {
-                  if (factionFilter === 'All') {
-                    return true;
-                  } 
-                  return knight.faction.name === factionFilter;
-                })
                 .map(knight => (
                   <Fragment key={knight.name}>
                     <tr>
                       <td>{knight.name}</td>
-                      <td>{knight.faction.name}</td>
+                      <td>{_.get(knight, 'faction.name')}</td>
+                      <td>{_.get(knight, 'startingClass.name')}</td>
+                      <td>{knight.startingLevel}</td>
+                      <td>{knight.growth}</td>
+                      <td>{knight.growthRate}</td>
+                      <td>{knight.startingHP}</td>
+                      <td>{knight.attack}</td>
+                      <td>{knight.defense}</td>
+                      <td>{knight.str}</td>
+                      <td>{knight.int}</td>
+                      <td>{knight.agi}</td>
+                      <td>{knight.manaPool}</td>
                       <td>{knight.maxManaPool}</td>
+                      <td>{knight.runeArea}</td>
                     </tr>
                   </Fragment>
                 ))}
@@ -100,8 +145,8 @@ Knightbrowser.defaultProps = {};
 
 Knightbrowser.propTypes = {};
 
-const mapStateToProps = state => ({});
+const mapStateToProps = () => ({});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Knightbrowser);
