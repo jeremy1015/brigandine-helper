ba490a27abfc48b75a35e7009c34de224a7c32fc
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-07-12 21:33:09 -0400

    Fixed mobile hamburger menu not displaying

--- src/features/navigation/index.jsx
+++ src/features/navigation/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Collapse,
   Navbar,
   NavbarBrand,
+  NavbarToggler,
   Nav,
   UncontrolledDropdown,
   DropdownToggle,
@@ -16,50 +17,56 @@ import { connect } from 'react-redux';
 import routes from '../../util/routes';
 import eliza from '../../img/eliza.png';
 
-const NavigationBar = () => (
-  <Navbar color="light" expand="sm">
-    <NavbarBrand>
-      <img src={eliza} alt="hide and seek" height="32px" />
+const NavigationBar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand>
+        <img src={eliza} alt="hide and seek" height="32px" />
       &nbsp;Brigandine Helper
-    </NavbarBrand>
-    <Collapse navbar>
-      <Nav className="mr-auto" navbar>
-        <UncontrolledDropdown inNavbar nav>
-          <DropdownToggle nav caret>
-            Useful Links
-          </DropdownToggle>
-          <DropdownMenu>
-            <NavLink target="_blank" href="https://discord.gg/8CF38Qe">
-              <DropdownItem>
-                Brigandine Discord
-              </DropdownItem>
+      </NavbarBrand>
+      <NavbarToggler onClick={toggleNavbar} color="light" className="mr-2" />
+      <Collapse isOpen={!collapsed} navbar>
+        <Nav className="mr-auto" navbar>
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle nav caret>
+              Useful Links
+            </DropdownToggle>
+            <DropdownMenu>
+              <NavLink target="_blank" href="https://discord.gg/8CF38Qe">
+                <DropdownItem>
+                  Brigandine Discord
+                </DropdownItem>
+              </NavLink>
+              <NavLink target="_blank" href="https://docs.google.com/spreadsheets/d/1phCX__6IuomaEnioApz_db_ZNaA7xwPazKwJoHDqhIY">
+                <DropdownItem>
+                  Runersia Stat Tracker by Lasci
+                </DropdownItem>
+              </NavLink>
+              <NavLink target="_blank" href="https://brigandine.fandom.com/wiki/Brigandine:_The_Legend_of_Runersia">
+                <DropdownItem>
+                  Brigandine Fandom Wiki
+                </DropdownItem>
+              </NavLink>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+          <NavItem>
+            <NavLink tag={Link} to={routes.questfinder}>
+              Location Questfinder
             </NavLink>
-            <NavLink target="_blank" href="https://docs.google.com/spreadsheets/d/1phCX__6IuomaEnioApz_db_ZNaA7xwPazKwJoHDqhIY">
-              <DropdownItem>
-                Runersia Stat Tracker by Lasci
-              </DropdownItem>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to={routes.knightbrowser}>
+              Knight Browser
             </NavLink>
-            <NavLink target="_blank" href="https://brigandine.fandom.com/wiki/Brigandine:_The_Legend_of_Runersia">
-              <DropdownItem>
-                Brigandine Fandom Wiki
-              </DropdownItem>
-            </NavLink>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-        <NavItem>
-          <NavLink tag={Link} to={routes.questfinder}>
-            Location Questfinder
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to={routes.knightbrowser}>
-            Knight Browser
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </Collapse>
-  </Navbar>
-);
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  ); 
+};
 
 NavigationBar.defaultProps = {};
 
